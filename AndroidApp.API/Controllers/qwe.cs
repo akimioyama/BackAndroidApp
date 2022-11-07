50eefc6515dcbe2100af6fa4c97066e8a1a1c416
Авраменко Дима <no.pers@bk.ru>
2022-11-07 14:38:50 +0300

    Добавьте файлы проекта.

---
+++ AndroidApp.API/Controllers/qwe.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+using AndroidApp.Domain.Person;
+
+namespace AndroidApp.API.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class qwe : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult allPerson()
+        {
+            string sql = $"select * from person";
+
+            string stringconn = "Data Source=LAPTOP-FPR118VN\\SQLEXPRESS;" +
+                "Initial Catalog=android;" +
+                "Integrated Security=True;" +
+                "Connect Timeout=30;" +
+                "Encrypt=False;" +
+                "TrustServerCertificate=False;" +
+                "ApplicationIntent=ReadWrite;" +
+                "MultiSubnetFailover=False";
+            SqlConnection conn = new SqlConnection(stringconn);
+            conn.Open();
+            var command = new SqlCommand(sql, conn);
+            var person = new List<Person>();
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var temp = new Person(
+                        (int)reader.GetInt32(0),
+                        reader.GetString(1).ToString(),
+                        reader.GetString(2).ToString()
+                        );
+                    person.Add(temp);
+
+                }
+            }
+            var json = JsonConvert.SerializeObject(person);
+
+            conn.Close();
+            return Ok(json);
+        }
+    }
+}
---
+++ AndroidApp.Domain/Person/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidApp.Domain.Person
+{
+    public class Person
+    {
+        public int id;
+        public string name;
+        public string number;
+        public Person(int id, string name, string number)
+        {
+            this.id = id;
+            this.name = name;
+            this.number = number;
+        }
+    }
+}
